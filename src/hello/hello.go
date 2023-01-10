7b2e5dad79325f00979978f38644d2126cc7bbd0
jjeanjacques10 <jjean.jacques10@gmail.com>
2023-01-09 21:49:36 -0300

    add menu

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -2,16 +2,22 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 )
 
 func main() {
 	name := "Barros"
-	age := 23
 	var version float32 = 1.1
 	fmt.Println("Hello World! Mr.", name)
-	fmt.Println("You are ", age, " years old") // Print age equals 0
 	fmt.Println("This software version is ", version)
 
-	fmt.Println("Get variable type: ", reflect.TypeOf(age))
+	fmt.Println("1- Start Monit")
+	fmt.Println("2- Show Logs")
+	fmt.Println("0- Exit")
+
+	var command int
+	fmt.Scan(&command)
+	//fmt.Scanf("%d", &command)
+
+	fmt.Println("The selected command was ", command)
+	fmt.Println("The address of my command variable is ", &command)
 }
