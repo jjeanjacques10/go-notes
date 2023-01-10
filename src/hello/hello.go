1f076701dc793e913e14e848b55accd143ec6b49
jjeanjacques10 <jjean.jacques10@gmail.com>
2023-01-09 22:14:39 -0300

    split in functions

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -2,22 +2,15 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	name := "Barros"
-	var version float32 = 1.1
-	fmt.Println("Hello World! Mr.", name)
-	fmt.Println("This software version is ", version)
 
-	fmt.Println("1- Start Monit")
-	fmt.Println("2- Show Logs")
-	fmt.Println("0- Exit")
+	showHelloWorld()
+	showMenu()
 
-	var command int
-	fmt.Scan(&command)
-
-	fmt.Println("The selected command was ", command)
+	command := readCommand()
 
 	switch command {
 	case 1:
@@ -26,8 +19,30 @@ func main() {
 		fmt.Println("Showing logs...")
 	case 0:
 		fmt.Println("Exit...")
+		os.Exit(0)
 	default:
 		fmt.Println("Command not found")
+		os.Exit(-1)
 	}
+}
 
+func showHelloWorld() {
+	name := "Barros"
+	var version float32 = 1.1
+	fmt.Println("Hello World! Mr.", name)
+	fmt.Println("This software version is ", version)
+}
+
+func showMenu() {
+	fmt.Println("1- Start Monit")
+	fmt.Println("2- Show Logs")
+	fmt.Println("0- Exit")
+}
+
+func readCommand() int {
+	var commandRead int
+	fmt.Scan(&commandRead)
+
+	fmt.Println("The selected command was ", commandRead)
+	return commandRead
 }
